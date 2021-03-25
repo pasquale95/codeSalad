383dea04c95bb3b589cb43966e2d84818f20cd64
Pasquale95 <pasqualeconvertini95@gmail.com>
2021-03-25 22:45:58 +0100

    wrapped chapter01 solutions

---
+++ src/chapter01/Main.java
@@ -0,0 +1,19 @@
+package chapter01;
+
+/**
+ * @author Pasquale Convertini <pasqualeconvertini95@gmail.com>
+ * @github @pasquale95
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        timeSolutions();
+    }
+
+    public static void timeSolutions() {
+        chapter01.ex1_1.Solution.time();
+        chapter01.ex1_2.Solution.time();
+    }
+}
