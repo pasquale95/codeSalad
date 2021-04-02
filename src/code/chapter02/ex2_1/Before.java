eda46c5be24a68aae0fa6da2335bbd4957838165
Pasquale95 <pasqualeconvertini95@gmail.com>
2021-04-02 09:03:47 +0200

    skeleton for ex2_1

---
+++ src/code/chapter02/ex2_1/Before.java
@@ -0,0 +1,19 @@
+package chapter02.ex2_1;
+
+/**
+ * @author Pasquale Convertini <pasqualeconvertini95@gmail.com>
+ * @github @pasquale95
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+public class Before implements Runnable {
+
+    public Before(String[] input) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/code/chapter02/ex2_1/Solution.java
@@ -0,0 +1,35 @@
+package chapter02.ex2_1;
+
+import utils.Timer;
+
+import static utils.Colors.colorYellow;
+import static utils.Colors.printBlue;
+
+/**
+ * @author Pasquale Convertini <pasqualeconvertini95@gmail.com>
+ * @github @pasquale95
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ */
+public class Solution {
+
+    private static final String PROBLEM = "Chapter 1. Ex 2.1";
+    private static final String[] strings = {"genetics", "salt", "castle", "controlled"};
+
+    /**
+     * Chapter 2
+     * TODO
+     */
+    public static void main(String[] args) {
+        printBlue(PROBLEM);
+        // TODO
+    }
+
+    public static void time() {
+        //Before before = new Before(strings);
+        //After after = new After(strings);
+
+        //Timer timer = new Timer(PROBLEM, before, after);
+        //timer.start();
+    }
+}
