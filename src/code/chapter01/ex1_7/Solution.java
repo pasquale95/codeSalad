da7f1e0e907ab3e72b5cf85222dd287e1025beaf
pasquale95 <pasqualeconvertini95@gmail.com>
2021-12-02 16:09:35 +0100

    UPDATE: fixed typo

--- src/code/chapter01/ex1_7/Solution.java
+++ src/code/chapter01/ex1_7/Solution.java
@@ -21,7 +21,7 @@ public class Solution extends SolutionTemplate {
 
     /**
      * Chapter 1
-     * Ex1.1: Rotate Matrix
+     * Ex1.7: Rotate Matrix
      * Given an image represented by an N x N matrix, where each pixel in the image
      * is represented by an integer, write a method to rotate the image by 90 degrees.
      * Can you do this in place?
